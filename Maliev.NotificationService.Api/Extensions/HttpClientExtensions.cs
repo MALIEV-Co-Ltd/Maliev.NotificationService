803b48da2c9e738abe0e0b0dc954cbac233863bc
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-05-04 07:49:40 +0700

    Use authenticated customer service client

--- Maliev.NotificationService.Api/Extensions/HttpClientExtensions.cs
+++ Maliev.NotificationService.Api/Extensions/HttpClientExtensions.cs
@@ -6,23 +6,9 @@ public static class NotificationHttpClientExtensions
 {
     public static IHostApplicationBuilder AddNotificationCustomerServiceClient(this IHostApplicationBuilder builder)
     {
-        builder.Services.AddHttpClient<ICustomerServiceClient, CustomerServiceClient>((sp, client) =>
-        {
-            var configuration = sp.GetRequiredService<IConfiguration>();
-            var explicitUrl = configuration["Services:CustomerService:BaseUrl"];
-
-            if (!string.IsNullOrEmpty(explicitUrl))
-            {
-                client.BaseAddress = new Uri(explicitUrl);
-            }
-            else
-            {
-                client.BaseAddress = new Uri($"http://CustomerService");
-            }
-
-            client.Timeout = TimeSpan.FromSeconds(90);
-        })
-        .AddServiceDiscovery();
+        builder.AddAuthenticatedServiceClient<ICustomerServiceClient, CustomerServiceClient>(
+            "CustomerService",
+            sourceServiceName: "notification");
 
         return builder;
     }
